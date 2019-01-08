d1c567bb729464e7db542a18f17f9896f23783f1
Marius Ropotica <marius.ropotica@basware.com>
2019-01-08 20:32:39 +0200

    configure CORS

--- infrastructure/signup.sh
+++ infrastructure/signup.sh
@@ -1,9 +1,9 @@
 aws cognito-idp sign-up \
     --region eu-west-1 \
-    --client-id 3dcc66b5e2lm1t5k4069tg7f91 \
+    --client-id 5r18p7b2kd8vss1fcaj949dnh5 \
     --username marius.ropotica+1@gmail.com \
     --password Passw0rd!
 
 aws cognito-idp admin-confirm-sign-up \
-    --user-pool-id eu-west-1_w7d0PcP3c \
+    --user-pool-id eu-west-1_KwPtm93tM \
     --username marius.ropotica+1@gmail.com
--- notes-client/src/config.js
+++ notes-client/src/config.js
@@ -2,12 +2,12 @@ export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   apiGateway: {        
     REGION: "eu-west-1",
-    URL: "https://ipa4fp2hw6.execute-api.eu-west-1.amazonaws.com/Prod"
+    URL: "https://1wb2u11o7g.execute-api.eu-west-1.amazonaws.com/Prod"
   },
   cognito: {
     REGION: "eu-west-1",
-    USER_POOL_ID: "eu-west-1_w7d0PcP3c",
-    APP_CLIENT_ID: "3dcc66b5e2lm1t5k4069tg7f91",
-    IDENTITY_POOL_ID: "eu-west-1:de1082c7-9154-471a-93c9-e05142585d24"
+    USER_POOL_ID: "eu-west-1_KwPtm93tM",
+    APP_CLIENT_ID: "5r18p7b2kd8vss1fcaj949dnh5",
+    IDENTITY_POOL_ID: "eu-west-1:f0fca4b3-244b-465e-b4bc-89fb111246ac"
   }
 };
