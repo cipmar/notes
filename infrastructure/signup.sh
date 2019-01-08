093033b6c70811a53a64662a3d74731deca665a7
Marius Ropotica <marius.ropotica@basware.com>
2019-01-08 21:58:56 +0200

    add one more user

--- infrastructure/signup.sh
+++ infrastructure/signup.sh
@@ -4,6 +4,16 @@ aws cognito-idp sign-up \
     --username marius.ropotica+1@gmail.com \
     --password Passw0rd!
 
+aws cognito-idp sign-up \
+    --region eu-west-1 \
+    --client-id 3vct6ajua5g22i75fp2a34t4vs \
+    --username marius.ropotica+2@gmail.com \
+    --password Passw0rd!
+
 aws cognito-idp admin-confirm-sign-up \
     --user-pool-id eu-west-1_JOjyGHXkw \
     --username marius.ropotica+1@gmail.com
+
+aws cognito-idp admin-confirm-sign-up \
+    --user-pool-id eu-west-1_JOjyGHXkw \
+    --username marius.ropotica+2@gmail.com
